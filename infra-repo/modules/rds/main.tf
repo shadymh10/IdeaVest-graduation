5c68a60fd97b721ee495c6ac21540ac4b18304b4
shadymh10 <154983287+shadymh10@users.noreply.github.com>
2026-04-04 01:43:55 +0200

    Update main.tf

--- infra-repo/modules/rds/main.tf
+++ infra-repo/modules/rds/main.tf
@@ -1,16 +1,13 @@
-# ============================================
-# RDS Module - PostgreSQL on Private Subnet
-# ============================================
-
 resource "aws_db_instance" "main" {
   identifier = "${var.project_name}-${var.environment}-db"
 
-  engine               = "postgres"
+  engine               = "sqlserver-ex"
   engine_version       = var.db_engine_version
   instance_class       = var.db_instance_class
   allocated_storage    = var.db_allocated_storage
   storage_type         = "gp3"
   storage_encrypted    = true
+  port                 = 1433
 
   db_name  = var.db_name
   username = var.db_username
@@ -19,7 +16,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = var.db_subnet_group_name
   vpc_security_group_ids = [var.database_sg_id]
 
-  multi_az            = false  # Free Tier
+  multi_az            = false
   publicly_accessible = false
   skip_final_snapshot = true
 
@@ -27,8 +24,7 @@ resource "aws_db_instance" "main" {
   backup_window           = "03:00-04:00"
   maintenance_window      = "Mon:04:00-Mon:05:00"
 
-  # Performance Insights (free for db.t3.micro)
-  performance_insights_enabled = var.db_instance_class == "db.t3.micro" ? true : false
+  performance_insights_enabled = false
 
   tags = {
     Name = "${var.project_name}-${var.environment}-db"
